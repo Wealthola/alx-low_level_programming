fa6ee70bd50174c103503a437ed88cbf6c803fdb
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-08 12:44:41 -0700

    ok

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,6 @@
-#include <stdio.h>
-/**
- * main - main block
- * Return: 0
- */
-int main(void)
+#include<stdio.h>
+int main()
 (
- puts("\"Programming is like building a multilingual puzzle\n");
- return (0);
+ puts("Programming is like building a multilingual puzzle\n");
+ return 0;
 )   
