14c347aac407bb4666a8198e2242cbe98c60815a
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-08 15:14:49 -0700

    ok

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,15 @@
 #include<stdio.h>
+#include<string.h>
+#include<unistd.h>
 /**
  * main - entry
  * Return: 1
  */
 int main(void)
 {
-	char str1="and that piece of art is useful\"-Dora Korpar, 2015-10-19\n";
-	write(2, str1, 59);
+	int s= strlen("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n");
+	write(2,
+			"and that piece of art is useful\" -Dora Korpar,2015-10-19\n",
+			s);
 	return (1);
 }
