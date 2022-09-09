079b088dd80d94a6cb7b8cf69b485b2639dd6fbf
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-09 13:54:27 -0700

    ok

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - print the alphabet in lowercase, folllowed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		put char(ch);
+	}
+	putchar('\n');
+	return (0);
+}
