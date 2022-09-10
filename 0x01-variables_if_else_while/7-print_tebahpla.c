df56081067490b9d87c19024c0866fcf86b27fd6
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-10 08:49:12 -0700

    ok

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,3 +1,16 @@
 #include<stdio.h>
 /**
- * main - 
+ * main - print reversed alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
