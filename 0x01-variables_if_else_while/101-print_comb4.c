33450ad8c1921ce9ec53c9ff85a9f0f4d336f2eb
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-10 11:04:55 -0700

    ok

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+/**
+ * main - print all posible different combination of three digit
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 36; i	< 50)
+	{
+		for (j = 37; j < 50)
+		{
+			for (k = 38; k < 50)
+			{
+				if (j > i && i > k)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (i != 40 || j != 41)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
 
