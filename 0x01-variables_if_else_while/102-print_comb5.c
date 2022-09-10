5fd23fb4def78622601eb764c1bc3b0afd36a3f2
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-10 10:45:15 -0700

    ok

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+/**
+ * main - print all possible combination of two-digit number
+ * Return: 0
+ */
+int main(void)
+{
+	int i, e;
+	int a, b, c, d;
+
+	for(i = 0; i < 100; i++)
+	{
+		a = i / 10; 
+		b = i % 10;
+
+		for (e = 0; e < 100; e++)
+		{
+			c = e / 10;
+			d = e % 10;
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(32);
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (!(a == 9 && b == 8))
+				{
+					putchar(44);
+					putchar(32);
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
