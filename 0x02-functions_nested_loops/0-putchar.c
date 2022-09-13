e03c7f761421e9cc32b737b1e355d71ab1eca770
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-13 11:16:28 -0700

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,14 +5,6 @@
  */
 int main(void)
 {
-	putchar("_");
-	putchar("p");
-	putchar("u");
-	putchar("t");
-	putchar("c");
-	putchar("h");
-	putchar("a");
-	putchar("r");
-	putchar('\n');
+	printf("_putchar\n");
 	return (0);
 }
