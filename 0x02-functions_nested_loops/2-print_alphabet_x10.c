f5c7d857c115977f2cd619cd20981a92a7305eb7
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-13 13:47:21 -0700

    ok

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * main - function to print alphabet x10
+ * Return: 0
+ */
+int main(void)
+{
+	char c, i;
+	for (i = 0; i <= 9; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putcharr(c);
+		}
+		_putchar('\n');
+	}
+}
