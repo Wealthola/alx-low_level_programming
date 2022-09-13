ee07d0f2889beb14c29bd9646470a1362f8db4d5
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-13 13:31:24 -0700

    ok

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,15 +5,14 @@
  */
 int main(void)
 {
-	_putchar("_");
-	_putchar("p");
-	_putchar("u");
-	_putchar("t");
-	_putchar("c");
-	_putchar("h");
-	_putchar("a");
-	_putchar("r");
-	_putchar('\n');
+	char word[] = "_putchar";
+	int i = 0;
 
+	while (i < (int)sizeof(word) - 1)
+	{
+		_putchar(word[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
