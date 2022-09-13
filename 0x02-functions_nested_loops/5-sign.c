8b9289f5fc40dc1df0718561542e2621ade5f37c
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-13 15:41:58 -0700

    ok

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print sign - function to check for a sign of a number
  * @n: is the int that will use for the argument of the function
- * Return: 0
+ * Return: 1 if positive, 0 if zero, -1 if negative
  */
 int print_sign(int n)
 {
@@ -11,14 +11,11 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
-	{
-		_putchar('-');
-		return (-1);
-	}
-	else
+	 if (n == 0)
 	{
 		_putchar('0');
 		return (0);
 	}
+		_putchar('-');
+		return (-1);
 }
