b0eb75fb82dff13472c8018ae148fc99b3a548f7
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-13 13:03:39 -0700

    ok

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+/**
+ * main - print alphabet
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchat(c);
+	}
+	_putchar('\n');
+	return (0);
+}
