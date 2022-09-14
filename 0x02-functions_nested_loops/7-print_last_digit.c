147c8912429fdc52e50e94f4accb6a7145c5fdbb
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-14 03:14:43 -0700

    ok

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - return the value of the last digit
+ * @n: integer as argument
+ * Return: return int
+ */
+int print_last_digit(int n)
+{
+	int reminder;
+
+	reminder = n % 10;
+	return (reminder);
+}
