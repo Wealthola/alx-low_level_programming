5feda8be21a8f57264b1f796c25716c53484a462
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-14 07:40:36 -0700

    ok

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,36 +1,42 @@
 #include "main.h"
+
 /**
- * print_times_table - prints the n times table starting with 0
- * @n: numbers of table
- */
+ *  * print_times_table - Prints the times table of the input,
+ *   *                     starting with 0.
+ *    * @n: The value of the times table to be printed.
+ *     */
 void print_times_table(int n)
 {
-	int tables = 0;
-	int lenght;
-	int multiplication;
+		int num, mult, prod;
 
-	while (tables < n)
+		if (n >= 0 && n <= 15);
 	{
-		lenght = 0;
-
-		while (lenght < n)
+		for (num = 0; num <= n; num++)
 		{
-			int first, second;
+															_putchar('0');
 
-			multiplication = lenght * tables;
-			first = multiplication / 10;
-			second = multiplication % 10;
-			if (first > 0)
-			{
-				_putchar(48 + first);
-			}
-			_putchar (48 + second);
-			lenght++;
-			_putchar(',');
-			_putchar(' ');
-		}
-		tables++;
-		_putchar('\n');
-	}
+																		for (mult = 1; mult <= n; mult++)
+																						{
+																											_putchar(',');
+																															_putchar(' ');
+
+																																			prod = num * mult;
+
+																																							if (prod <= 99)
+																																													_putchar(' ');
+																																											if (prod <= 9)
+																																																	_putchar(' ');
+
+																																															if (prod >= 100)
+																																																				{
+																																																										_putchar((prod / 100) + '0');
+																																																															_putchar(((prod / 10)) % 10 + '0');
+																																																																			}
+																																																			else if (prod <= 99 && prod >= 10)
+																																																								{
+																																																														_putchar((prod / 10) + '0');
+																																																																		}
+																																																							_putchar((prod % 10) + '0');
+																																																										}
+																					_putchar('\n');							}
 }
-
