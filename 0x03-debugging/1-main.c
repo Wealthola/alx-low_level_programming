345402646880666fc8368f6b85cd63414bf13f6c
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-15 03:37:47 -0700

    ok

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - causes an indefinite loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("indefiniteloop incoming :(\n");
+
+	i = 0;
+	/**
+	 * while (i < 10)
+	 * {
+	 * putchar(i);
+	 * }
+	 */
+	printf("indefinite loop avoided! \\o/\n");
+
+	return (0);
+}
