5f9ca59eab846f7389b1e0f0819752f5433d3854
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-15 05:04:28 -0700

    ok

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
 /**
- * print_remaining_days - takes a date and prints how maony days are left in the year, taking leap into account
+ * print_remaining_days - takes a date and prints how many days
+ *  are left in the year, taking leap into account
+ *
  * @month: month in number format
  * @day: day of month
  * @year: year
