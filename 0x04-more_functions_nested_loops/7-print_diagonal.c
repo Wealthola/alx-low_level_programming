181c919bd2646034bd59eb2ad2af8b63b090741b
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-15 12:46:39 -0700

    ok

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,13 +5,23 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+	if (n <= 0)
 	{
-		_putchar(" ");
-		_putchar("\\");
 		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < n; j++)
+			{
+			if (j == i)
+				_putchar('\\');
+			else if (j < i)
+				_putchar(' ');
+			}
+		_putchar('\n');
+		}
 	}
-	_putchar('\n');
 }
