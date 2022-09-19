8ea4d9a9be52db53c692af28fe7835bae32fda94
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-19 13:22:34 -0700

    ok

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,10 +11,10 @@ char *_strcpy(char *dest, char *src)
 
 	while (*(src + inc) != '\0')
 	{
-		*(dest + inc) = *(scr + inc);
+		*(dest + inc) = *(src + inc);
 		inc++;
 	}
-	*dest +inc) = '\0';
+	*dest +inc ) = '\0';
 
 	return (dest);
 }
