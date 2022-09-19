075dd9e3fc8c2242f3021919dea332998ddd7e30
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-19 13:18:16 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _strcpy - copy and paste
+ * @dest: destruction
+ * @src: source
+ * Return: destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int inc = 0;
+
+	while (*(src + inc) != '\0')
+	{
+		*(dest + inc) = *(scr + inc);
+		inc++;
+	}
+	*dest +inc) = '\0';
+
+	return (dest);
+}
