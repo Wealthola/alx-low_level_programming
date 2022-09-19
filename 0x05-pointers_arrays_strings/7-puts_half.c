1e4683eae1379abd161f9b26984c79f298f7f258
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-19 12:39:29 -0700

    ok

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string
+ * Return: half string
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	while (*str != '\0')
+	{
+		lens++;
+		str++;
+	}
+	str -= (len / 2);
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
