5cf59211fccbe059fb668abde795af45a204630c
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-09-27 08:14:13 -0700

    k

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,9 +7,18 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, sum1 = 0, sum2 = 0;
+	int i, j, p, l = 0, r = 0;
 
-	for (j = 0, i = size - 1; j < (size * size); j += size + 1, i += size - 1)
-		sum1 += a[j], sum2 += a[i];
-	printf("%d, %d\n", sum1, sum2);
+	for (i = 0; i < size; i++)
+	{
+		p = (i * size) + i;
+		l += *(a + p);
+	}
+	for (j = 0; j < size; j++)
+	{
+		p = (j * size) + (size - 1 - j);
+		r += *(a + p);
+	}
+	printf("%i, %i\n", l, r);
+}
 }
