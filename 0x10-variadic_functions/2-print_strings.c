1e3b8d290de3b09b5963d175ce1bc8f3f7fe840d
olaitan majekodunmi <majekodunmimercy@gmail.com>
2022-10-14 09:00:21 -0700

    ok

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings - A function that print strings followed by a new line.
+ * @separator: An input string to be printed between numbers.
+ * @n: number of parameters
+ * ap = Majek, i = Olaitan, string = Wealth (Declaration).
+ * Return: Nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list Majek;
+	unsigned int Olaitan = 0;
+	char *Wealth;
+
+	va_start(Majek, n);
+	for (; Olaitan < n; Olaitan++)
+	{
+		Wealth = va_arg(Wealth, char*);
+		if (Wealth == NULL)
+			printf("(nil)");
+		else
+			printf("%s", Wealth);
+		if (separator == NULL)
+			continue;
+		if (Olaitan < n - 1)
+			printf("%s", separator);
+	}
+	printf("\n");
+	va_end(Majek);
+}
